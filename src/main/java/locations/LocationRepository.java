470d63ffdf1b994de468a7c4b2d5a8e959b05056
Sam Khawase <sam.khawase@gmail.com>
2018-03-14 11:16:35 +0100

    Added Map.html which POSTs the location details in the REST application

--- src/main/java/locations/LocationRepository.java
+++ src/main/java/locations/LocationRepository.java
@@ -3,7 +3,9 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "location", path = "location")
 public interface LocationRepository extends PagingAndSortingRepository<Location, Long> {
     List<Location> findByLocationName(@Param("name") String name);
